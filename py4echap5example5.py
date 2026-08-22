01bfe14170e5f8c823a36aab709e2fc775c1dbea
ma1gnan <119422502+ma1gnan@users.noreply.github.com>
2026-08-22 09:08:06 -0400

    Add files via upload

---
+++ py4echap5example5.py
@@ -0,0 +1,19 @@
+#2:33:33
+
+# loop idoms
+
+# print('Before')
+# for thing in [9, 41, 12, 3, 74, 15] :
+#     print(thing)
+# print('After')
+
+# What is the largest number
+#2:37:35
+
+largest_so_far = -1
+print('Before', largest_so_far)
+for the_num in [9, 41, 12, 3, 74, 15] :
+    if the_num > largest_so_far :
+        largest_so_far = the_num
+
+print('After', largest_so_far)
